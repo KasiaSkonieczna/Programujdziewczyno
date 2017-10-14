04c7ff8bec50361300fd540605510f9886c1c3cf
KasiaSkonieczna <kasia.skonieczna@o2.pl>
2017-10-14 16:38:19 +0200

    show more or less buttons

--- js/app.js
+++ js/app.js
@@ -21,5 +21,26 @@ document.addEventListener('DOMContentLoaded', function() { // działa dopiero gd
 
       forDropdown.addEventListener('mouseover', showDropdown);
       forDropdown.addEventListener('mouseout', hideDropdown);
-      
-    });
+
+      // read-more and read-less
+
+      // Read more/less
+  var buttons = document.querySelectorAll('.read-more');
+
+  function showHide() {
+
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
+});
